0827d3474be9b58ddfd0586c5e9384ee6187ad45
Kevin Atkinson <k@kevina.org>
2016-05-06 04:58:23 -0400

    Move AdvReader into its own file.
    License: MIT
    Signed-off-by: Kevin Atkinson <k@kevina.org>

--- commands/files/file.go
+++ commands/files/file.go
@@ -11,57 +11,6 @@ var (
 	ErrNotReader    = errors.New("This file is a directory, can't use Reader functions")
 )
 
-// An AdvReader is like a Reader but supports getting the current file
-// path and offset into the file when applicable.
-type AdvReader interface {
-	io.Reader
-	ExtraInfo() ExtraInfo
-	SetExtraInfo(inf ExtraInfo) error
-}
-
-type ExtraInfo interface {
-	Offset() int64
-	AbsPath() string
-	// Clone creates a copy with different offset
-	Clone(offset int64) ExtraInfo
-}
-
-type PosInfo struct {
-	offset  int64
-	absPath string
-}
-
-func (i PosInfo) Offset() int64 { return i.offset }
-
-func (i PosInfo) AbsPath() string { return i.absPath }
-
-func (i PosInfo) Clone(offset int64) ExtraInfo { return PosInfo{offset, i.absPath} }
-
-func NewPosInfo(offset int64, absPath string) PosInfo {
-	return PosInfo{offset, absPath}
-}
-
-type advReaderAdapter struct {
-	io.Reader
-}
-
-func (advReaderAdapter) ExtraInfo() ExtraInfo {
-	return nil
-}
-
-func (advReaderAdapter) SetExtraInfo(_ ExtraInfo) error {
-	return errors.New("Reader does not support setting ExtraInfo.")
-}
-
-func AdvReaderAdapter(r io.Reader) AdvReader {
-	switch t := r.(type) {
-	case AdvReader:
-		return t
-	default:
-		return advReaderAdapter{r}
-	}
-}
-
 // File is an interface that provides functionality for handling
 // files/directories as values that can be supplied to commands. For
 // directories, child files are accessed serially by calling `NextFile()`.
@@ -105,13 +54,4 @@ type SizeFile interface {
 	File
 
 	Size() (int64, error)
-}
-
-type PosInfoWaddOpts struct {
-	ExtraInfo
-	AddOpts interface{}
-}
-
-func (i PosInfoWaddOpts) Clone(offset int64) ExtraInfo {
-	return PosInfoWaddOpts{i.ExtraInfo.Clone(offset), i.AddOpts}
 }
