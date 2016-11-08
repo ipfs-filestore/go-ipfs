dc98ab76755820869fc1807b1921c6ae0813967d
Kevin Atkinson <k@kevina.org>
2016-11-07 19:12:43 -0500

    Remove safepath dependency.
    License: MIT
    Signed-off-by: Kevin Atkinson <k@kevina.org>

--- filestore/support/blockstore.go
+++ filestore/support/blockstore.go
@@ -10,8 +10,7 @@ import (
 	fs_pb "github.com/ipfs/go-ipfs/unixfs/pb"
 	cid "gx/ipfs/QmXfiyr2RWEXpVDdaYnD2HNiBk6UBddsvEP4RPfXb6nGqY/go-cid"
 	ds "gx/ipfs/QmbzuUusHqaLLoNTDEVLcSF6vZDHZDLPC7p4bztRvvkXxU/go-datastore"
-
-	"gx/ipfs/QmRpAnJ1Mvd2wCtwoFevW8pbLTivUqmFxynptG6uvp1jzC/safepath"
+	"path/filepath"
 )
 
 type blockstore struct {
@@ -121,7 +120,7 @@ func (bs *blockstore) prepareBlock(k ds.Key, block b.Block) (*DataObj, error) {
 			return nil, fmt.Errorf("%s: %s: no stat information for file", block.Cid(), posInfo.FullPath)
 		}
 		d := &DataObj{
-			FilePath: safepath.Clean(posInfo.FullPath),
+			FilePath: filepath.Clean(posInfo.FullPath),
 			Offset:   posInfo.Offset,
 			Size:     uint64(fileSize),
 			ModTime:  FromTime(posInfo.Stat.ModTime()),
